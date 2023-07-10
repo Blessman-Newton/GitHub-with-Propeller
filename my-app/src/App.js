64fbdcbe7f3307b41e30af1cd45ea71b4d8bbb16
Blessman-Newton <blessmannewton0@gmail.com>
2023-07-10 17:19:21 +0100

    Add link & logo

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://www.propelleraero.com/dirtmate/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Propelleraero
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
