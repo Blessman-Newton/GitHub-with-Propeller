da6040c85b3457feb562925097ce29423ea4bbc1
Blessman-Newton <blessmannewton0@gmail.com>
2023-07-10 17:46:44 +0100

    Add command.sh

---
+++ command.sh
@@ -0,0 +1,9 @@
+#create react app
+npx create-react-app my-app
+git commit -m "initial"
+#create a new branch for the logo changes
+git checkout -b update_logo
+git commit -m "Add link & logo"
+push --set-upstream origin update_logo
+
+#REPO_URL https://github.com/Blessman-Newton/GitHub-with-Propeller
